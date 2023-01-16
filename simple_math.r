01b04132ee8d062c266046d4c615846ac3b0a917
Krishna1081 <90314846+Krishna1081@users.noreply.github.com>
2023-01-16 22:34:59 +0530

    Added simple_math.r file

---
+++ simple_math.r
@@ -0,0 +1,12 @@
+print(paste("Addition: ", 10 + 5))
+print(paste("Subtraction: ", 10 - 5))
+
+# Built in functions
+print(paste("Max: ", max(5, 10, 15)))
+print(paste("Min: ", min(5, 10, 15)))
+
+print(paste("Square root: ", sqrt(16)))
+print(paste("absolute: ", abs(-4.7)))
+
+print(paste("ceiling: ", ceiling(1.4)))
+print(paste("floor: ", floor(1.4)))
