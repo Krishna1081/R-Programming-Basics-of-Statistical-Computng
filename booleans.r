2943ff212dd0ff5bed038c5ed24308cf0db84017
Krishna1081 <90314846+Krishna1081@users.noreply.github.com>
2023-01-16 22:49:10 +0530

    Added booleans.r file

---
+++ booleans.r
@@ -0,0 +1,21 @@
+# Example 1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# Example 2
+a <- 10
+b <- 9
+print(a > b)
+
+# Example 3
+a <- 200
+b <- 33
+
+# Example 4
+if (b > a) {
+    print("b is greater than a")
+} else {
+    print("b is not greater than a")
+
+}
