4f40002613fe596638615929847b685d52ae2803
Krishna1081 <90314846+Krishna1081@users.noreply.github.com>
2023-01-16 22:11:03 +0530

    Added datatypes file

---
+++ datatypes.r
@@ -0,0 +1,26 @@
+# Example 1: changing of variable types
+my_var <- 30
+print(typeof(my_var))
+my_var <- "Sally"
+print(typeof(my_var))
+
+# Basic Data Types
+# numeric
+x <- 10.5
+print(class(x))
+
+# integer
+x <- 1000L
+print(class(x))
+
+# complex
+x <- -9i + 3
+print(class(x))
+
+# string or character
+x <- "R is exciting"
+print(class(x))
+
+# logical or boolean
+x <- TRUE
+print(class(x))
