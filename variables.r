51e96b0460123070a60683028d02f7ec223a69d9
Krishna1081 <90314846+Krishna1081@users.noreply.github.com>
2023-01-16 22:04:07 +0530

    Added variables file

---
+++ variables.r
@@ -0,0 +1,22 @@
+# Example 1
+name <- "John"
+age <- 40
+print(name)
+print(age)
+
+# Example 2
+text1 <- "R is"
+text2 <- "awesome"
+text3 <- paste(text1, text2)
+print(text3)
+
+# Example 3
+num1 <- 5
+num2 <- 10
+print(num1 + num2)
+
+# Example 4
+var1 <- var2 <- var3 <- "Orange"
+print(var1)
+print(var2)
+print(var3)
